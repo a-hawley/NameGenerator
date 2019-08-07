8232356d14f542fdb9ba826cb3157b4de5cb10a7
Alec J Hawley <alech35@yahoo.com>
2019-08-07 19:13:16 -0400

    Finished roughdraft of program.

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,28 +1,46 @@
+# Imports
 from peewee import *
 import random
-import time
+from time import sleep
+from os import system, name
 
+
+# Create peewee database
 db = SqliteDatabase('names.db')
 
 
+# Set character length and uniqueness
 class PastName(Model):
     PastName = CharField(max_length=255, unique=True)
 
+# Make your database connection
     class Meta:
         database = db
 
 
-if __name__  ==  '__main__':
+# Make sure you can run multiple times without breaking peewee
+if __name__ == '__main__':
     db.connect()
     db.create_tables([PastName], safe=True)
 
-def sleep():
-    time.sleep(1)
 
+# Purely aesthetic
+def pause():
+    sleep(1)
+
+
+# Purely aesthetic
 def load():
     for i in range(3):
         print("Loading...")
-        time.sleep(.500)
+        sleep(.500)
+
+
+# Purely aesthetic
+def clear():
+    # os.system('cls' if os.name == 'nt' else 'clear') usually works for clearing the screen,
+    # but not for PyCharm. Instead it inserts an arrow like symbol.
+    print("\n" * 10)
 
 
 first_names = [
@@ -47,8 +65,8 @@ first_name = input("Enter your first name only:\n")
 last_name = input("Enter your last name only:\n")
 
 load()
-sleep()
-
+pause()
+clear()
 print("""
 Your name has been entered into our machine, 
 and is being analyzed for a suitable dystopian name.
@@ -58,8 +76,9 @@ new_first_name = random.choice(first_names)
 new_last_name = random.choice(last_names)
 new_name = new_first_name + " " + new_last_name
 
-sleep()
+pause()
 load()
-sleep()
+pause()
+clear()
 
-print(f"In a destroyed world, your name would be {new_name}")
+print(f"In a destroyed world, your name would be {new_name}!")
